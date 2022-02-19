ea6074b0a3d91b62cca4cd57b81864a004ceeb3b
ryowuandjanet <ryowu0329@gmail.com>
2022-02-20 00:46:46 +0800

    initial

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return ( 
+    <footer>
+      <div>Copyright 2022 Hinja List</div>
+    </footer>
+   );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+  return ( 
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keyword" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>ryowu</p>
+        <p>janet</p>
+      </div>
+    </>
+   );
+}
+ 
+export default About;
